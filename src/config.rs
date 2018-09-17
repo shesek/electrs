08ee6e9df84f8259576837f78d7cedbfaf09b599
Riccardo Casatta <riccardo@casatta.it>
2018-09-17 10:44:56 -0700

    add get block and query examples

--- src/config.rs
+++ src/config.rs
@@ -61,7 +61,7 @@ impl Config {
             .arg(
                 Arg::with_name("network")
                     .long("network")
-                    .help("Select Bitcoin network type ('mainnet', 'testnet' or 'regtest')")
+                    .help("Select Bitcoin network type ('mainnet', 'testnet', 'regtest', 'liquid', 'liquidregtest')")
                     .takes_value(true),
             )
             .arg(
